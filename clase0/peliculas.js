b6838b4f7576c5dc502c6972c2c10dfeb0b1bfaf
Damcarrizo <dami.c@live.com.ar>
2023-01-03 20:23:12 -0300

    clase0

---
+++ clase0/peliculas.js
@@ -0,0 +1,10 @@
+/*declaramos variables*/
+
+let pelicula = "Toy Story"
+let serie = "The Office"
+const serie2 = "Breaking Bad"
+
+/*mostramos variables en consola*/
+console.log(pelicula);
+console.log(serie);
+console.log(serie2);
